f57064ebf3311721f916bb41c096e33f7e00bcce
Eliasggabr <elias.menezes1@aluno.ce.gov.br>
2026-02-12 16:13:00 -0300

    Add files via upload

---
+++ q2.php
@@ -0,0 +1,21 @@
+<?php
+
+$x = 100;
+$y = 5;
+
+$soma = $x + $y;
+$subtracao = $x - $y;
+$multiplicacao = $x * $y;
+$divisao = $x / $y;
+
+echo "soma: $soma\n";
+echo "subtração: $subtracao\n";
+echo "multiplicação: $multiplicacao\n";
+echo "divisão: $divisao\n";
+
+
+
+
+
+
+?>
